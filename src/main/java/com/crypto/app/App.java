72ac991fc4e7c3c95e92c60015dd679fe550cd35
mazucanti <daniel.mazucanti.ismart@gmail.com>
2023-07-04 23:16:28 -0300

    Finished the handling of the requests and calculation of information needed

--- src/main/java/com/crypto/app/App.java
+++ src/main/java/com/crypto/app/App.java
@@ -1,11 +1,84 @@
 package com.crypto.app;
-import java.util.List;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.json.JSONObject;
 
 public class App {
+
+    public static String final_msg = "total=%.2f,best_asset=%s,best_performance=%.2f,worst_asset=%s,worst_performance=%.2f";
+
+    public static String getFinal_msg() {
+        return final_msg;
+    }
+
+    public static void setFinal_msg(String final_msg) {
+        App.final_msg = final_msg;
+    }
+
     public static void main(String[] args) {
-        Wallet wallet = new Wallet("src/main/resources/wallet.csv");
-        List<String[]> w = wallet.getWalletValues();
-        System.out.println("Hello World!");
+        Wallet wallet = new Wallet();
+        Double walletTotal = 0.0;
+        Double worstAsset = Double.MAX_VALUE;
+        Double bestAsset = Double.MIN_VALUE;
+        Integer bestIdx = -1;
+        Integer worstIdx = -1;
+        String filePath = "src/main/resources/wallet.csv";
+        wallet.loadFile(filePath);
+
+        List<Double> currentPrices = runRequests(wallet);
+        System.out.println(currentPrices);
+
+        for (int i = 0; i < currentPrices.size(); i++) {
+            Double performance = currentPrices.get(i) / wallet.getWalletPrices().get(i);
+            walletTotal += currentPrices.get(i) * wallet.getWalletQuantity().get(i);
+
+            if (worstAsset > performance) {
+                worstAsset = performance;
+                worstIdx = i;
+            }
+            if (bestAsset < performance) {
+                bestAsset = performance;
+                bestIdx = i;
+            }
+        }
+        String results = String.format(getFinal_msg(),
+                walletTotal,
+                wallet.getWalletSymbols().get(bestIdx),
+                bestAsset,
+                wallet.getWalletSymbols().get(worstIdx),
+                worstAsset);
+        setFinal_msg(results);
+        System.out.println(results);
+    }
+
+    private static List<Double> runRequests(Wallet wallet) {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        List<String> symbols = wallet.getWalletSymbols();
+        List<Double> currentPrices = new ArrayList<>(symbols.size());
+        for (int i = 0; i < symbols.size(); i++) {
+            String symbol = symbols.get(i);
+            Callable<String> callable = new Comms(symbol);
+            try {
+                String response = executor.submit(callable).get();
+                JSONObject jsonObj = new JSONObject(response);
+                Double currentPrice = jsonObj.getJSONArray("data").getJSONObject(0).getDouble("priceUsd");
+                currentPrices.add(i, currentPrice);
+                String msg = String.format("The price of %s is USD %.2f", symbol, currentPrice);
+                System.out.println(msg);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+
+        }
+        executor.shutdown();
+        return currentPrices;
     }
 }
--- src/test/java/com/crypto/app/AppTest.java
+++ src/test/java/com/crypto/app/AppTest.java
@@ -4,17 +4,13 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void anwserCorrect() {
+        String expectedMsg = "total=101652.89,best_asset=BCH,best_performance=58.32,worst_asset=XRP,worst_performance=0.00";
+        App.main(null);
+        String finalMessage = App.getFinal_msg();
+        assertTrue(expectedMsg.equals(finalMessage));
     }
+
 }
