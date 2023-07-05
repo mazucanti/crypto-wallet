0983e0200d5c57cdce2479b948bc2ce28181c85f
mazucanti <daniel.mazucanti.ismart@gmail.com>
2023-07-04 23:15:25 -0300

    Finished parsing class to store wallet info

--- src/main/java/com/crypto/app/Wallet.java
+++ src/main/java/com/crypto/app/Wallet.java
@@ -1,111 +1,78 @@
 package com.crypto.app;
 
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
 
 public class Wallet {
 
     // Class variables
-    private String[] walletSymbols;
+    private List<String> walletSymbols;
 
-    private double[] walletPrices;
+    private List<Double> walletPrices;
 
-    private double[] walletQuantity;
+    private List<Double> walletQuantity;
 
-    private double[] currentPrices;
-
-    private float[] upliftValue;
-
-    public List<String[]> walletValues;
-
-    public List<String[]> getWalletValues() {
-        return walletValues;
-    }
-
-    public double[] getWalletPrices() {
-        return walletPrices;
-    }
-
-    public void setWalletPrices(double[] walletPrices) {
-        this.walletPrices = walletPrices;
-    }
-
-    public double[] getWalletQuantity() {
-        return walletQuantity;
-    }
-
-    public void setWalletQuantity(double[] walletQuantity) {
-        this.walletQuantity = walletQuantity;
-    }
-
-    public double[] getCurrentPrices() {
-        return currentPrices;
-    }
-
-    public void setCurrentPrices(double[] currentPrices) {
-        this.currentPrices = currentPrices;
-    }
-
-    public void setWalletValues(List<String[]> walletValues) {
-        this.walletValues = walletValues;
-    }
-
-    // Sea of getters and setters
-    public String[] getWalletSymbols() {
+    public List<String> getWalletSymbols() {
         return walletSymbols;
     }
 
-    public void setWalletSymbols(String[] walletSymbols) {
+    public void setWalletSymbols(List<String> walletSymbols) {
         this.walletSymbols = walletSymbols;
     }
 
-    public float[] getUpliftValue() {
-        return upliftValue;
+    public List<Double> getWalletPrices() {
+        return walletPrices;
     }
 
-    public void setUpliftValue(float[] upliftValue) {
-        this.upliftValue = upliftValue;
+    public void setWalletPrices(List<Double> walletPrices) {
+        this.walletPrices = walletPrices;
     }
 
-    // Constructor
-    public Wallet(String filePath) {
-        setWalletValues(readCSVFile(filePath));
-        parseWalletData();
+    public List<Double> getWalletQuantity() {
+        return walletQuantity;
     }
 
-    private void parseWalletData() {
-        List<String[]> walletValues = getWalletValues();
-        List<String> symbols = new ArrayList<String>();
-        List<Double> quantities = new ArrayList<Double>();
-        List<Double> prices = new ArrayList<Double>();
-
-        if (walletValues != null) {
-            for (String[] value : walletValues) {
-                symbols.add(value[0]);
-                quantities.add(Double.parseDouble(value[1]));
-                prices.add(Double.parseDouble(value[2]));
-            }
-        }
+    public void setWalletQuantity(List<Double> walletQuantity) {
+        this.walletQuantity = walletQuantity;
     }
 
-    private List<String[]> readCSVFile(String filePath) {
+    public void loadFile(String filePath) {
+
+        System.out.println("Loading csv...");
+
+        String line = "";
+        String splitBy = ",";
+
+        List<Double> prices = new ArrayList<>();
+        List<Double> quantities = new ArrayList<>();
+        List<String> symbols = new ArrayList<>();
+
         try {
-            Reader reader = Files.newBufferedReader(Paths.get(filePath));
-            CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(1).build();
-            List<String[]> wallet_values = csvReader.readAll();
-            return wallet_values;
-        } catch (Exception IOException) {
-            String msg = "File not found!\n" + IOException.getMessage();
-            System.out.print(msg);
-            return null;
-        }
+            // parsing a CSV file into BufferedReader class constructor
 
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
+
+            while ((line = br.readLine()) != null) // returns a Boolean value
+            {
+
+                if (!(line.contains("symbol") || line.contains("quantity") || line.contains("price"))) {
+
+                    String[] tuple = line.split(splitBy); // use comma as separator
+
+                    symbols.add(tuple[0]);
+                    quantities.add(Double.parseDouble(tuple[1]));
+                    prices.add(Double.parseDouble(tuple[2]));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        setWalletPrices(prices);
+        setWalletQuantity(quantities);
+        setWalletSymbols(symbols);
     }
 
 }
